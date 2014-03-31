cfd51af2a38ef4e90630f2bc28eff5e4581a202a
MrPrez <gagneret@yahoo.fr>
2014-03-31 23:44:13 +0200

    first commit

---
+++ src/main/java/com/mrprez/gencross/impl/pavillonNoir/PavillonNoir.java
@@ -0,0 +1,22 @@
+package com.mrprez.gencross.impl.pavillonNoir;
+
+import com.mrprez.gencross.Personnage;
+
+public class PavillonNoir extends Personnage {
+	
+	
+	
+	
+	@Override
+	public void calculate() {
+		super.calculate();
+		
+	}
+
+	
+
+	
+	
+	
+
+}
---
+++ src/test/java/com/mrprez/gencross/impl/pavillonNoir/test/StandardPavillonNoirPersonnageTest.java
@@ -0,0 +1,12 @@
+package com.mrprez.gencross.impl.pavillonNoir.test;
+
+import com.mrprez.gencross.test.StandardPersonnageTest;
+
+public class StandardPavillonNoirPersonnageTest extends StandardPersonnageTest {
+
+	public StandardPavillonNoirPersonnageTest() {
+		super("BSG");
+
+	}
+
+}
