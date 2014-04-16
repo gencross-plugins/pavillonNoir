bda872c4e9b9cff06c0cc473530fbd7708a2bc34
MrPrez <gagneret@yahoo.fr>
2014-04-16 02:24:57 +0200

    Correct phase issue

--- src/main/java/com/mrprez/gencross/impl/pavillonNoir/MigrationFrom1_0.java
+++ src/main/java/com/mrprez/gencross/impl/pavillonNoir/MigrationFrom1_0.java
@@ -1,5 +1,8 @@
 package com.mrprez.gencross.impl.pavillonNoir;
 
+import java.lang.reflect.Field;
+
+import com.mrprez.gencross.Personnage;
 import com.mrprez.gencross.Version;
 import com.mrprez.gencross.migration.MigrationPersonnage;
 import com.mrprez.gencross.migration.Migrator;
@@ -9,12 +12,14 @@ public class MigrationFrom1_0 implements Migrator {
 	@Override
 	public MigrationPersonnage migrate(MigrationPersonnage migrationPersonnage) throws Exception {
 		if(migrationPersonnage.getPhase().equals("création")){
-			migrationPersonnage.setPassword("Création");
+			Field phaseField = Personnage.class.getDeclaredField("phase");
+			phaseField.setAccessible(true);
+			phaseField.set(migrationPersonnage, "Création");
 		}
 		migrationPersonnage.getProperty("Compétences#Religion").getSubProperties().getDefaultProperty().setEditable(false);
 		migrationPersonnage.getProperty("Compétences#Art").getSubProperties().getDefaultProperty().setEditable(false);
-		migrationPersonnage.getProperty("Langue étrangère").getSubProperties().getDefaultProperty().setEditable(false);
-		migrationPersonnage.getPluginDescriptor().setVersion(new Version(1,1));
+		migrationPersonnage.getProperty("Compétences#Langue étrangère").getSubProperties().getDefaultProperty().setEditable(false);
+		migrationPersonnage.getPluginDescriptor().setVersion(new Version(1,2));
 		return migrationPersonnage;
 	}
 
