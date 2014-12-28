db84c1a33f1e6cef21235be1d0301b5491d70e98
mrprez <gagneret@yahoo.fr>
2014-12-28 12:07:54 +0100

    Add traces for debug

--- src/test/java/com/mrprez/gencross/impl/pavillonNoir/test/TestMigrationFrom1_6.java
+++ src/test/java/com/mrprez/gencross/impl/pavillonNoir/test/TestMigrationFrom1_6.java
@@ -5,6 +5,11 @@ import com.mrprez.gencross.test.MigratorTest;
 public class TestMigrationFrom1_6 extends MigratorTest {
 
 	public TestMigrationFrom1_6() {
+		Personnage personnage = personnageFactory.loadPersonnage(ClassLoader.getSystemResourceAsStream(fileName));
+		System.out.println("personnage loaded");
+		PersonnageSaver.savePersonnageXml(personnage, getResultFile());
+		System.out.println("personnage saved in "+getResultFile().getAbsolutePath());
+		Assert.assertTrue(getRefFile().getAbsolutePath()+" does not exist", getRefFile().exists());
 		super("Esclave_marron.xml");
 	}
 
