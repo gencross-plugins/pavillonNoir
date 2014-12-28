e072dae89299cd47f4763c3ba3156e502493b70c
mrprez <gagneret@yahoo.fr>
2014-12-28 12:13:11 +0100

    Undo trace adding

--- src/test/java/com/mrprez/gencross/impl/pavillonNoir/test/TestMigrationFrom1_6.java
+++ src/test/java/com/mrprez/gencross/impl/pavillonNoir/test/TestMigrationFrom1_6.java
@@ -2,19 +2,10 @@ package com.mrprez.gencross.impl.pavillonNoir.test;
 
 import com.mrprez.gencross.test.MigratorTest;
 
-import com.mrprez.gencross.Personnage;
-import com.mrprez.gencross.disk.PersonnageFactory;
-import com.mrprez.gencross.disk.PersonnageSaver;
-
 
 public class TestMigrationFrom1_6 extends MigratorTest {
 
 	public TestMigrationFrom1_6() {
-		Personnage personnage = personnageFactory.loadPersonnage(ClassLoader.getSystemResourceAsStream(fileName));
-		System.out.println("personnage loaded");
-		PersonnageSaver.savePersonnageXml(personnage, getResultFile());
-		System.out.println("personnage saved in "+getResultFile().getAbsolutePath());
-		Assert.assertTrue(getRefFile().getAbsolutePath()+" does not exist", getRefFile().exists());
 		super("Esclave_marron.xml");
 	}
 
