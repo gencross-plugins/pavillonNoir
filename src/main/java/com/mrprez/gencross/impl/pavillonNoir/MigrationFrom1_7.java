e6ec5369eec9b9895c556ede041ce4337eeb4bd3
MrPrez <guillaume.gagneret@gmail.com>
2015-01-04 14:15:08 +0100

    Correct migration

--- src/main/java/com/mrprez/gencross/impl/pavillonNoir/MigrationFrom1_7.java
+++ src/main/java/com/mrprez/gencross/impl/pavillonNoir/MigrationFrom1_7.java
@@ -1,6 +1,7 @@
 package com.mrprez.gencross.impl.pavillonNoir;
 
 import com.mrprez.gencross.Version;
+import com.mrprez.gencross.formula.Formula;
 import com.mrprez.gencross.migration.MigrationPersonnage;
 import com.mrprez.gencross.migration.Migrator;
 
@@ -8,6 +9,10 @@ public class MigrationFrom1_7 implements Migrator {
 
 	@Override
 	public MigrationPersonnage migrate(MigrationPersonnage migrationPersonnage) throws Exception {
+		migrationPersonnage.getFormulaManager().removeFormula("Esquive");
+		migrationPersonnage.getFormulaManager().removeFormula("Vie");
+		migrationPersonnage.getFormulaManager().addFormula(new Formula("Esquive=#Attributs#Réflexe# * 4 + 4"));
+		migrationPersonnage.getFormulaManager().addFormula(new Formula("Vie=#Attributs#Carrure# * 2 + 4"));
 		migrationPersonnage.getFormulaManager().impactModificationFor("Attributs#Carrure", migrationPersonnage);
 		migrationPersonnage.getFormulaManager().impactModificationFor("Attributs#Réflexe", migrationPersonnage);
 		
