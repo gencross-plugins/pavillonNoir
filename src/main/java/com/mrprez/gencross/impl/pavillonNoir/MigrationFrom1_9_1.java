78886c8f2bc7846cc29edbb14e50b94efa84ce44
mrprez <gagneret@yahoo.fr>
2015-06-02 13:32:59 +0200

    Add "3" in attribut list

--- src/main/java/com/mrprez/gencross/impl/pavillonNoir/MigrationFrom1_9_1.java
+++ src/main/java/com/mrprez/gencross/impl/pavillonNoir/MigrationFrom1_9_1.java
@@ -3,6 +3,7 @@ package com.mrprez.gencross.impl.pavillonNoir;
 import com.mrprez.gencross.PropertiesList;
 import com.mrprez.gencross.Property;
 import com.mrprez.gencross.Version;
+import com.mrprez.gencross.value.StringValue;
 import com.mrprez.gencross.migration.MigrationPersonnage;
 import com.mrprez.gencross.migration.Migrator;
 
@@ -13,13 +14,13 @@ public class MigrationFrom1_9_1 implements Migrator {
 	public MigrationPersonnage migrate(MigrationPersonnage migrationPersonnage) throws Exception {
 		migrationPersonnage.getPluginDescriptor().setVersion(new Version(1, 9, 2));
 		
-		defaultCompetence = migrationPersonnage.getProperty("Compétences").getSubProperties().getDefaultProperty();
+		Property defaultCompetence = migrationPersonnage.getProperty("Compétences").getSubProperties().getDefaultProperty();
 		
-		Property attribut1 = new Property("Attribut 1", new StringValue("Erudition"), defaultCompetence, true);
+		Property attribut1 = new Property("Attribut 1", new StringValue("3"), defaultCompetence, true);
 		attribut1.setOptions(new String[]{"Erudition", "Perception", "Pouvoir", "Carrure", "Dextérité", "Réflexe", "Social", "3"});
 		defaultCompetence.getSubProperties().add(attribut1);
 		
-		Property attribut2 = new Property("Attribut 2", new StringValue("Erudition"), defaultCompetence, true);
+		Property attribut2 = new Property("Attribut 2", new StringValue("3"), defaultCompetence, true);
 		attribut2.setOptions(new String[]{"Erudition", "Perception", "Pouvoir", "Carrure", "Dextérité", "Réflexe", "Social", "3"});
 		defaultCompetence.getSubProperties().add(attribut2);
 		
