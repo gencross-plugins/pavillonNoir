e9f18f69022474e56038462467a7fce7ba9be869
mrprez <gagneret@yahoo.fr>
2015-06-02 10:26:20 +0200

    New version

--- src/main/java/com/mrprez/gencross/impl/pavillonNoir/MigrationFrom1_9_1.java
+++ src/main/java/com/mrprez/gencross/impl/pavillonNoir/MigrationFrom1_9_1.java
@@ -1,1 +1,19 @@
+package com.mrprez.gencross.impl.pavillonNoir;
 
+import com.mrprez.gencross.PropertiesList;
+import com.mrprez.gencross.Property;
+import com.mrprez.gencross.Version;
+import com.mrprez.gencross.migration.MigrationPersonnage;
+import com.mrprez.gencross.migration.Migrator;
+
+
+public class MigrationFrom1_9_1 implements Migrator {
+
+	@Override
+	public MigrationPersonnage migrate(MigrationPersonnage migrationPersonnage) throws Exception {
+		migrationPersonnage.getPluginDescriptor().setVersion(new Version(1, 9, 2));
+		return migrationPersonnage;
+	}
+	
+	
+}
