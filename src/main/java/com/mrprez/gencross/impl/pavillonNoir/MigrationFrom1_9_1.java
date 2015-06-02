12a361b9e25b0b1b1f91e2a473968f52d8c86b9b
mrprez <gagneret@yahoo.fr>
2015-06-02 12:32:54 +0200

    Add "3" in attribut list

--- src/main/java/com/mrprez/gencross/impl/pavillonNoir/MigrationFrom1_9_1.java
+++ src/main/java/com/mrprez/gencross/impl/pavillonNoir/MigrationFrom1_9_1.java
@@ -12,6 +12,17 @@ public class MigrationFrom1_9_1 implements Migrator {
 	@Override
 	public MigrationPersonnage migrate(MigrationPersonnage migrationPersonnage) throws Exception {
 		migrationPersonnage.getPluginDescriptor().setVersion(new Version(1, 9, 2));
+		
+		defaultCompetence = migrationPersonnage.getProperty("Compétences").getSubProperties().getDefaultProperty();
+		
+		Property attribut1 = new Property("Attribut 1", new StringValue("Erudition"), defaultCompetence, true);
+		attribut1.setOptions(new String[]{"Erudition", "Perception", "Pouvoir", "Carrure", "Dextérité", "Réflexe", "Social", "3"});
+		defaultCompetence.getSubProperties().add(attribut1);
+		
+		Property attribut2 = new Property("Attribut 2", new StringValue("Erudition"), defaultCompetence, true);
+		attribut2.setOptions(new String[]{"Erudition", "Perception", "Pouvoir", "Carrure", "Dextérité", "Réflexe", "Social", "3"});
+		defaultCompetence.getSubProperties().add(attribut2);
+		
 		return migrationPersonnage;
 	}
 	
