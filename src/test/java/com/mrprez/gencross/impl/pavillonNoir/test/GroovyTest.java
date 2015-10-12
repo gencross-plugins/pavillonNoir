5b39f8eeefca1f9c1265049e4724212bf3d1b9c8
MrPrez <guillaume.gagneret@gmail.com>
2015-10-13 00:37:58 +0200

    Get work

---
+++ src/test/java/com/mrprez/gencross/impl/pavillonNoir/test/GroovyTest.java
@@ -0,0 +1,22 @@
+package com.mrprez.gencross.impl.pavillonNoir.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.mrprez.gencross.Personnage;
+import com.mrprez.gencross.Property;
+import com.mrprez.gencross.disk.PersonnageFactory;
+import com.mrprez.gencross.value.IntValue;
+
+public class GroovyTest {
+	
+	@Test
+	public void testChangeApprentissage() throws Exception{
+		PersonnageFactory personnageFactory = new PersonnageFactory();
+		Personnage personnage = personnageFactory.buildNewPersonnage("Pavillon Noir");
+		Property apprentissage = personnage.getProperty("Compétences#Balistique#Apprentissage");
+		personnage.setNewValue(apprentissage, new IntValue(2));
+		Assert.assertEquals(4, personnage.getProperty("Compétences#Balistique").getValue().getInt());
+	}
+
+}
